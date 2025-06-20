a9b61bd83ab91540ebcda77e7b25803b834b23ea
Amashagit <amasharamawickrama@gmail.com>
2025-06-20 15:47:02 +0530

    changes

---
+++ src/component/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <div className="leftSide">
+            <img src={Logo} alt="Cake"/>
+        </div>
+        <div className="rightSide">
+            <Link to="/">Home</Link>
+            <Link to="/menu">Menu</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/review">Review</Link>
+            <button>
+                <ReorderIcon />
+            </button>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
