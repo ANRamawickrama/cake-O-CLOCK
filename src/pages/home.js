c1c88f2657c7a7f3868982a20157f15dcc0163f2
Amashagit <amasharamawickrama@gmail.com>
2025-06-21 06:43:04 +0530

    rebuild

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Menu } from "@mui/material";
+import React from "react";
+import {Link} from "react-router-dom";
+import BannerImage from "../assets/home.jpg";
+import "../styles/Home.css" ;
 
 function home() {
   return (
-    <div>Hello World</div>
+    <div className="home">
+      <div className="headerContainer"
+       style={{backgroundImage: 'url(${BannerImage})'}}>
+        <h1>CAKE'O CLOCK</h1>
+        <Link to="/menu">
+          <button>ORDER NOW</button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
