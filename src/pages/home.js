a25c904f2f85ba3a00844aca9685d2275bc89e94
Amashagit <amasharamawickrama@gmail.com>
2025-06-21 07:01:03 +0530

    home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,22 @@
-import { Menu } from "@mui/material";
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import BannerImage from "../assets/home.jpg";
-import "../styles/Home.css" ;
+import "../styles/Home.css";
 
-function home() {
+function Home() {
   return (
     <div className="home">
-      <div className="headerContainer"
-       style={{backgroundImage: 'url(${BannerImage})'}}>
+      <div
+        className="headerContainer"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      >
         <h1>CAKE'O CLOCK</h1>
         <Link to="/menu">
           <button>ORDER NOW</button>
         </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default home;
+export default Home;
